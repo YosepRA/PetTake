d395d6e71cbcd8f90866095eefc8781cc843c0e8
YosepRA <yosep.ryan.yra@gmail.com>
2021-07-14 19:16:15 +0700

    Add user register and login API.

--- api/app.js
+++ api/app.js
@@ -2,26 +2,51 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
 
 const mongoConnect = require('./mongoConnect');
-const imageRouter = require('./routes/image');
 const startApolloServer = require('./apiHandler');
+const passport = require('./passport');
+
+const imageRouter = require('./routes/image');
+const userRouter = require('./routes/user');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+let sessionSecret = process.env.SESSION_SECRET;
+if (!sessionSecret) {
+  console.log(
+    'You are using unsafe session secret. Provide application with safe secret using environment variable.',
+  );
+  sessionSecret = 'unsafe_secret';
+}
 
 mongoConnect();
 
 // ===== Midldewares ======
 
 app.use(express.json());
-app.use(express.static('public'));
 app.use(cors());
+app.use(
+  session({
+    secret: sessionSecret,
+    saveUninitialized: false,
+    resave: false,
+  }),
+);
+
+app.use(express.static('public'));
+
+/* ========== Passport initialization ========== */
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // ===== Routes ======
 
 startApolloServer(app);
 app.use('/image', imageRouter);
+app.use('/user', userRouter);
 
 app.listen(PORT, () => {
   console.log(`API server is listening on port ${PORT}...`);
--- api/models/User.js
+++ api/models/User.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
   name: { type: String, required: true },
   phone: { type: String, required: true },
   address: { type: String, required: true },
   pets: [{ type: Schema.Types.ObjectId, ref: 'Pet' }],
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', userSchema);
